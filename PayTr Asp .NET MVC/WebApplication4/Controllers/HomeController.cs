74b62ab54cffd97260df4eca434d784270285623
Salih YILDIZ <63597572+salihyildiz08@users.noreply.github.com>
2022-11-18 16:06:25 +0300

    Asp .Net Mvc Paytr Entegrasyonu

---
+++ PayTr Asp .NET MVC/WebApplication4/Controllers/HomeController.cs
@@ -0,0 +1,273 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+using System.Web.Script.Serialization;
+
+namespace WebApplication4.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Paytr(int tutar, string mail, string ad, string tel, string aciklama)
+        {
+            // ####################### DÜZENLEMESİ ZORUNLU ALANLAR #######################
+            //
+            // API Entegr     asyon Bilgileri - Mağaza paneline giriş yaparak BİLGİ sayfasından alabilirsiniz.
+            string merchant_id = "314546";
+            string merchant_key = "oBPWn6gffNwR11R2";
+            string merchant_salt = "rNd5M6bQ71ZZ4j6d";
+            //
+            // Müşterinizin sitenizde kayıtlı veya form vasıtasıyla aldığınız eposta adresi
+            string emailstr = mail;
+            //
+            // Tahsil edilecek tutar. 9.99 için 9.99 * 100 = 999 gönderilmelidir.
+            int payment_amountstr = tutar * 100;
+            //
+            // Sipariş numarası: Her işlemde benzersiz olmalıdır!! Bu bilgi bildirim sayfanıza yapılacak bildirimde geri gönderilir.
+            Random rand = new Random();
+            var rand1 = rand.Next(50000000, 100000000);
+            string merchant_oid = (rand1).ToString();
+            //
+            // Müşterinizin sitenizde kayıtlı veya form aracılığıyla aldığınız ad ve soyad bilgisi
+            string user_namestr = ad;
+            //
+            // Müşterinizin sitenizde kayıtlı veya form aracılığıyla aldığınız adres bilgisi
+            string user_addressstr = "Nizip";
+            //
+            // Müşterinizin sitenizde kayıtlı veya form aracılığıyla aldığınız telefon bilgisi
+            string user_phonestr = tel;
+            //
+            // Başarılı ödeme sonrası müşterinizin yönlendirileceği sayfa
+            // !!! Bu sayfa siparişi onaylayacağınız sayfa değildir! Yalnızca müşterinizi bilgilendireceğiniz sayfadır!
+            // !!! Siparişi onaylayacağız sayfa "Bildirim URL" sayfasıdır (Bakınız: 2.ADIM Klasörü).
+            var domain = Request.Url.GetLeftPart(UriPartial.Authority);
+            string merchant_ok_url = "http://dilektest.online/home/bildirim";
+            //
+            // Ödeme sürecinde beklenmedik bir hata oluşması durumunda müşterinizin yönlendirileceği sayfa
+            // !!! Bu sayfa siparişi iptal edeceğiniz sayfa değildir! Yalnızca müşterinizi bilgilendireceğiniz sayfadır!
+            // !!! Siparişi iptal edeceğiniz sayfa "Bildirim URL" sayfasıdır (Bakınız: 2.ADIM Klasörü).
+            HtmlHelper helper = new HtmlHelper(new ViewContext(ControllerContext, new WebFormView(ControllerContext, "Index"), new ViewDataDictionary(), new TempDataDictionary(), new System.IO.StringWriter()), new ViewPage());
+          
+            string merchant_fail_url = "http://www.siteniz.com/basarisiz";
+            //        
+            // !!! Eğer bu örnek kodu sunucuda değil local makinanızda çalıştırıyorsanız
+            // buraya dış ip adresinizi (https://www.whatismyip.com/) yazmalısınız. Aksi halde geçersiz paytr_token hatası alırsınız.
+            string user_ip = Request.ServerVariables["HTTP_X_FORWARDED_FOR"];
+            if (user_ip == "" || user_ip == null)
+            {
+                // user_ip = Request.ServerVariables["REMOTE_ADDR"];
+                user_ip = "91.93.192.106";
+            }
+
+
+            // İşlem zaman aşımı süresi - dakika cinsinden
+            string timeout_limit = "30";
+            //
+            // Hata mesajlarının ekrana basılması için entegrasyon ve test sürecinde 1 olarak bırakın. Daha sonra 0 yapabilirsiniz.
+            string debug_on = "1";
+            //
+            // Mağaza canlı modda iken test işlem yapmak için 1 olarak gönderilebilir.
+            string test_mode = "1";
+            //
+            // Taksit yapılmasını istemiyorsanız, sadece tek çekim sunacaksanız 1 yapın
+            string no_installment = "0";
+            //
+            // Sayfada görüntülenecek taksit adedini sınırlamak istiyorsanız uygun şekilde değiştirin.
+            // Sıfır (0) gönderilmesi durumunda yürürlükteki en fazla izin verilen taksit geçerli olur.
+            string max_installment ="2";
+            //
+            // Para birimi olarak TL, EUR, USD gönderilebilir. USD ve EUR kullanmak için kurumsal@paytr.com 
+            // üzerinden bilgi almanız gerekmektedir. Boş gönderilirse TL geçerli olur.
+            string currency = "TL";
+            //
+            // Türkçe için tr veya İngilizce için en gönderilebilir. Boş gönderilirse tr geçerli olur.
+            string lang = "";
+
+            object[][] user_basket = { };
+
+
+            // Gönderilecek veriler oluşturuluyor
+            NameValueCollection data = new NameValueCollection();
+            data["merchant_id"] = merchant_id;
+            data["user_ip"] = user_ip;
+            data["merchant_oid"] = merchant_oid;
+            data["email"] = emailstr;
+            data["payment_amount"] = payment_amountstr.ToString();
+            //
+            // Sepet içerği oluşturma fonksiyonu, değiştirilmeden kullanılabilir.
+            JavaScriptSerializer ser = new JavaScriptSerializer();
+            string user_basket_json = ser.Serialize(user_basket);
+            string user_basketstr = Convert.ToBase64String(Encoding.UTF8.GetBytes(user_basket_json));
+            data["user_basket"] = user_basketstr;
+            //
+            // Token oluşturma fonksiyonu, değiştirilmeden kullanılmalıdır.
+            string Birlestir = string.Concat(merchant_id, user_ip, merchant_oid, emailstr, payment_amountstr.ToString(), user_basketstr, no_installment, max_installment, currency, test_mode, merchant_salt);
+            HMACSHA256 hmac = new HMACSHA256(Encoding.UTF8.GetBytes(merchant_key));
+            byte[] b = hmac.ComputeHash(Encoding.UTF8.GetBytes(Birlestir));
+            data["paytr_token"] = Convert.ToBase64String(b);
+            var tokenn = Convert.ToBase64String(b);
+
+            data["debug_on"] = debug_on;
+            data["test_mode"] = test_mode;
+            data["no_installment"] = no_installment;
+            data["max_installment"] = max_installment;
+            data["user_name"] = user_namestr;
+            data["user_address"] = user_addressstr;
+            data["user_phone"] = user_phonestr;
+            data["merchant_ok_url"] = merchant_ok_url;
+            data["merchant_fail_url"] = merchant_fail_url;
+            data["timeout_limit"] = timeout_limit;
+            data["currency"] = currency;
+            data["lang"] = lang;
+
+
+            using (WebClient client = new WebClient())
+            {
+                client.Headers.Add("Content-Type", "application/x-www-form-urlencoded");
+                byte[] result = client.UploadValues("https://www.paytr.com/odeme/api/get-token", "POST", data);
+                string ResultAuthTicket = Encoding.UTF8.GetString(result);
+                dynamic json = JValue.Parse(ResultAuthTicket);
+
+
+                if (json.status == "success")
+                {
+                    Session["tokenn"] = "https://www.paytr.com/odeme/guvenli/" + json.token;
+                }
+                else
+                {
+                    Response.Write("PAYTR IFRAME failed. reason:" + json.reason + "");
+                }
+
+            }
+
+
+            return View();
+        }
+
+
+
+
+        public ActionResult Bildirim()
+        {
+
+
+            return Redirect("/Home");
+
+        }
+
+
+
+
+        [System.Web.Mvc.HttpPost]
+        public ActionResult Bildirim(object a)
+        {
+            string merchant_key = "oBPWn6gffNwR11R2";
+            string merchant_salt = "rNd5M6bQ71ZZ4j6d";
+            string merchant_oid = Request.Form["merchant_oid"];
+            string status = Request.Form["status"];
+            string total_amount = Request.Form["total_amount"];
+            string hash = Request.Form["hash"];
+
+ 
+            string Birlestir = string.Concat(merchant_oid, merchant_salt, status, total_amount);
+            HMACSHA256 hmac = new HMACSHA256(Encoding.UTF8.GetBytes(merchant_key));
+            byte[] b = hmac.ComputeHash(Encoding.UTF8.GetBytes(Birlestir));
+            string token = Convert.ToBase64String(b);
+
+
+
+            //
+            // Oluşturulan hash'i, paytr'dan gelen post içindeki hash ile karşılaştır (isteğin paytr'dan geldiğine ve değişmediğine emin olmak için)
+            // Bu işlemi yapmazsanız maddi zarara uğramanız olasıdır.
+            if (hash.ToString() != token)
+            {
+                Response.Write("PAYTR notification failed: bad hash");
+                return Redirect("");
+            }
+
+            //###########################################################################
+
+            // BURADA YAPILMASI GEREKENLER
+            // 1) Siparişin durumunu $post['merchant_oid'] değerini kullanarak veri tabanınızdan sorgulayın.
+            // 2) Eğer sipariş zaten daha önceden onaylandıysa veya iptal edildiyse  echo "OK"; exit; yaparak sonlandırın.
+
+            if (status == "success")
+            { //Ödeme Onaylandı
+
+                // Bildirimin alındığını PayTR sistemine bildir.  
+                Response.Write("OK");
+                Session["token"] = token;
+
+                // BURADA YAPILMASI GEREKENLER ONAY İŞLEMLERİDİR.
+                // 1) Siparişi onaylayın.
+                // 2) iframe çağırma adımında merchant_oid ve diğer bilgileri veri tabanınıza kayıp edip bu aşamada karşılaştırarak eğer var ise bilgieri çekebilir ve otomatik sipariş tamamlama işlemleri yaptırabilirsiniz.
+                // 2) Eğer müşterinize mesaj / SMS / e-posta gibi bilgilendirme yapacaksanız bu aşamada yapabilirsiniz. Bu işlemide yine iframe çağırma adımında merchant_oid bilgisini kayıt edip bu aşamada sorgulayarak verilere ulaşabilirsiniz.
+                // 3) 1. ADIM'da gönderilen payment_amount sipariş tutarı taksitli alışveriş yapılması durumunda
+                // değişebilir. Güncel tutarı Request.Form['total_amount'] değerinden alarak muhasebe işlemlerinizde kullanabilirsiniz.
+
+                MailMessage mail = new MailMessage();
+                mail.To.Add("salih.yildiz@dilekhali.com");
+                mail.From = new MailAddress("salih.yildiz@dilekhali.com");
+                mail.Subject = "Paytr Geliyor";
+                mail.Body = hash;
+                mail.IsBodyHtml = true;
+
+
+                SmtpClient smtp = new SmtpClient("smtp.office365.com", 587);
+                smtp.Credentials = new NetworkCredential("salih.yildiz@dilekhali.com", "Aa123456");
+                smtp.EnableSsl = true;
+                smtp.Send(mail);
+
+
+            }
+            else
+            { //Ödemeye Onay Verilmedi
+
+                // Bildirimin alındığını PayTR sistemine bildir.  
+                Response.Write("OK");
+
+                // BURADA YAPILMASI GEREKENLER
+                // 1) Siparişi iptal edin.
+                // 2) Eğer ödemenin onaylanmama sebebini kayıt edecekseniz aşağıdaki değerleri kullanabilirsiniz.
+                // $post['failed_reason_code'] - başarısız hata kodu
+                // $post['failed_reason_msg'] - başarısız hata mesajı
+            }
+        
+
+            return View();
+
+        }
+
+
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
